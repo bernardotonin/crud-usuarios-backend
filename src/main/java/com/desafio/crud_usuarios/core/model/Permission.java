62a06b7a09d0246fe8109fe6e7ad7afe138d50ac
bernardotonin <btprates2004@gmail.com>
2024-12-16 16:27:55 -0300

    feat: create permission entity

---
+++ src/main/java/com/desafio/crud_usuarios/core/model/Permission.java
@@ -0,0 +1,51 @@
+package com.desafio.crud_usuarios.core.model;
+
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "permissions")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "permission", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<User> users;
+
+    public Permission() {}
+
+    public Permission(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
+
